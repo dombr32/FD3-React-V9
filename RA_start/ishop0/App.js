a5e38982cffedfaa02356342b8abeb32c71798ac
dombr32 <dombrrom@gmail.com>
2024-09-26 19:50:35 +0300

    FD3_ishop0

---
+++ RA_start/ishop0/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import IShopHead from './components/ishopHead';
+
+ReactDOM.render(
+  <IShopHead question="Интернет-магазин с доставкой по всей Беларуси" />, 
+  document.getElementById('container') 
+);
---
+++ RA_start/ishop0/components/ishopHead.js
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+
+import './ishopHead.css';
+
+class IShopHead extends React.Component {
+
+  render() {
+    return (
+      <div className='IShopHead'>
+        <span className='IShopHead_Text'>
+          {this.props.question}
+        </span>
+      </div>
+    );
+  }
+
+}
+
+export default IShopHead;
