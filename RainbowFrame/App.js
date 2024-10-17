e6cfdf152a22ec95f010bb9a82448047bfdf4871
dombr32 <dombrrom@gmail.com>
2024-10-17 23:28:48 +0300

    RainbowFrame

---
+++ RainbowFrame/App.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+
+
+let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <div>
+    <RainbowFrame colors={colors}>
+      Hello!
+    </RainbowFrame>
+  </div>,
+  
+
+  document.getElementById('container') 
+);
+
