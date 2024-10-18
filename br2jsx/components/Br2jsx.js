a2773f7fe9be0309112387482984f8b386f95af9
dombr32 <dombrrom@gmail.com>
2024-10-18 17:54:41 +0300

    Br2jsx

---
+++ br2jsx/components/Br2jsx.js
@@ -0,0 +1,35 @@
+﻿import React, { Fragment } from 'react';
+
+
+
+class Br2jsx extends React.Component {
+
+  br = (text) => {
+
+    let textArr = text.split(/<.+?>/); //создаем массив, где элементом будет каждое отделльное слово (в регулярном выражении берем любой тег)
+    let result=[]; //создаем массив для результата иттерации 
+
+    for (let i in textArr) {
+      if (i<textArr.length-1)
+      result.push(textArr[i], <br key="i"></br>) //складывае в массив слова уже с тегом <br>
+      else result.push(textArr[i])
+      // console.log(i)
+    }
+     return (
+      // text.replace(/<.+?>/g,"<br>")
+      result
+     )
+  }
+
+  render() {    
+       
+      return (
+      <div style={{backgroundColor:"#2F4F4F", color:"white", padding:"10px 10px", width: "max-content"}}>{this.br(this.props.text)}</div>
+      )
+  }
+
+}
+
+
+
+export default Br2jsx;
